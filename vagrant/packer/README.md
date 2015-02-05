e13f45c3e4f4b0c3f14ed384189d1300a5b59880
Bryan Saunders <btsaunde@gmail.com>
2015-02-04 21:55:29 -0500

    Cleaned up Vagrant Directory, Modified Packer to Compress VM so it can be used without Vagrant

--- vagrant/packer/README.md
+++
@@ -1,2 +0,0 @@
-# Packer Scripts
-These scripts are used to Generate the Fedora 20 Base Box for Vagrant.
--- vagrant/packer/scripts/fedora-20/gnome.sh
+++
@@ -1,4 +0,0 @@
-yum -y groups install "GNOME Desktop"
-ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
-reboot
-sleep 60
