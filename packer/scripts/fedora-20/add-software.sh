9efa1ff9e1300a249e18825c1dc502595df41097
Bryan Saunders <bsaunder@redhat.com>
2015-04-03 11:21:57 -0400

    Added Migration Tools

--- packer/scripts/fedora-20/add-software.sh
+++ packer/scripts/fedora-20/add-software.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
+echo "Reading config.properties..."
+. /home/vagrant/config.properties
+
 ## Java 1.7
 echo "Installing Java 1.7"
 yum -y install java-1.7.0-openjdk
@@ -15,3 +18,25 @@ yum -y install git
 yum -y install maven
 yum -y install subversion
 yum -y install ant
+
+## JBoss Dev Studio
+echo "Installing JBDS"
+mkdir /opt/jboss
+chown -R $vm_user:$vm_user /opt/jboss/
+sudo java -jar /home/vagrant/filesToCopy/productInstallers/jbdevstudio-product-universal-7.1.1.GA-v20140314-2145-B688.jar /home/vagrant/InstallJbds.xml
+
+## Installing Windup
+echo "Downloading Windup"
+wget http://download.jboss.org/windup/windup-cli-0.6.8.zip
+cp windup-cli-0.6.8.zip /home/$vm_user/installers
+rm -f windup-cli-0.6.8.zip
+
+## Installing WindRide
+echo "Downloading WindRide"
+wget https://repository.jboss.org/nexus/content/repositories/releases/org/jboss/migr/as/WindRide/1.0.0/WindRide-1.0.0.jar
+cp WindRide-1.0.0.jar /home/$vm_user/installers
+rm -f WindRide-1.0.0.jar
+
+## Set Ownership
+chown -R $vm_user:$vm_user /home/$vm_user/installers
+chown -R $vm_user:$vm_user /opt/jboss/
--- packer/scripts/fedora-20/cleanup.sh
+++ packer/scripts/fedora-20/cleanup.sh
@@ -4,4 +4,5 @@ yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 rm -f /home/vagrant/config.properties
 rm -f /home/vagrant/settings.xml
+rm -f /home/vagrant/InstallJbds.xml
 rm -rf /home/vagrant/filesToCopy
